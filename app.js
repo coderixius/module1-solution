51470be2dfa94b6f340af07867713585036d1d59
sakis papadopoulos <you@example.com>
2020-12-30 21:59:16 +0200

    Final Exercise

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-(function () {
-'use strict';
+(function() {
+	'use strict';
+	angular.module('LunchCheck', [])
+	.controller('LunchCheckController', LunchCheckController);
 
-angular.module('LunchCheck', [])
-.controller('LunchCheckController', LunchCheckController);
-
-function LunchCheckController($scope,
-                              $filter,
-                              $injector){
-
-  $scope.food = " ";
-
-  $scope.upper=function () {
-
-
-  };
+	LunchCheckController.$inject= ['$scope'];
+	function LunchCheckController($scope) {
+		$scope.message="";
+		$scope.foods="";
+		$scope.totalfoods=0;
 
 
 
-console.log($injector.annonate(LunchCheckController));
-}
+		$scope.countfoods= function(message) {
 
-function Annonate(textbox,button,placeholder){
-  return "Enjoy";
-}
+			console.log("message "+message)
+			if(message==="")
+				return 0;
+			var count=0;
+			var array= message.split(',');
+			for (var i = 0; i < array.length; i++) {
+				if(array[i].trim()!=="")
+					count++;
+			}
+			return count;
+		};
 
-console.log(LunchCheckController.toString());
+		$scope.showMessage= function() {
+			$scope.totalfoods= $scope.countItems($scope.items)
+			$scope.setData($scope.totalfoods);
+			if($scope.totalfoods<1)
+				$scope.message="Please enter data first";
+
+			else if($scope.totalfoods<4 &&
+					$scope.totalfoods>0)
+				$scope.message="Enjoy!";
+			else
+				$scope.message="Too much!"
+		};
+		
+
+
+
+	};
 
 })();
